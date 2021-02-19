c813bc2d6b504006bd9aa07a7a8bc275dde5ac7a
yinshiwei <1559524770@qq.com>
2021-02-19 12:31:05 +0800

    按需加载

--- craco.config.js
+++ craco.config.js
@@ -1,13 +1,32 @@
 const CracoLessPlugin = require('craco-less');
-
+const {getThemeVariables} = require('antd/dist/theme');
+const compactTheme = getThemeVariables({
+    // dark: true, // 开启暗黑模式
+    compact: true, // 开启紧凑模式
+})
 module.exports = {
+    webpack: {},
+    babel: {
+        plugins: [
+            [
+                "import",
+                {
+                    "libraryName": "antd",
+                    "libraryDirectory": "es",
+                    "style": true //设置为true即是less
+                },
+            ]
+        ]
+    },
     plugins: [
         {
             plugin: CracoLessPlugin,
             options: {
                 lessLoaderOptions: {
                     lessOptions: {
+
                         modifyVars: {
+                            ...compactTheme,
                             '@primary-color': '#EF4566'
                         },
                         javascriptEnabled: true,
